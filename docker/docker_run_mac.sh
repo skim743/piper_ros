9160497246604a4989c4989ee9ecd3fa1a9b7eba
Soobum Kim <skim743@gatech.edu>
2026-03-12 15:48:11 -0400

    Clean up code

--- docker/docker_run_mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it \
-           --rm \
-           --gpus all \
-           --name piper_ros \
-           -e DISPLAY=host.docker.internal:0 \
-           -v "$(pwd)/../data:/app/data" \
-           piper_ros_sim:humble
